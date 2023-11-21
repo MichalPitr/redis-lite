239f2daf7529f068170ae5c1408a1375817fd368
michal pitr <michal.pitr.altis@gmail.com>
2023-11-21 23:31:23 +0100

    add simple strings, errors, and integers

--- redis.go
+++ redis.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func deserialize() {
 	return
@@ -21,10 +24,69 @@ func deserializeSimpleString(message string) (string, int, error) {
 	return message[start:i], i - start, nil
 }
 
+func serializeSimpleString(message string) (string, int, error) {
+	for _, c := range message {
+		if c == '\n' || c == '\r' {
+			return "", 0, fmt.Errorf("CLRF characters are not allowed in simple strings.")
+		}
+	}
+	return fmt.Sprintf("+%s\r\n", message), len(message), nil
+}
+
+func deserializeSimpleError(message string) (string, int, error) {
+	if message[0] != '-' {
+		return "", 0, fmt.Errorf("Expected a simple error startin with '-' but got '%c'\n", message[0])
+	}
+
+	start := 1
+	i := start
+	for message[i] != '\r' && message[i+1] != '\n' {
+		i++
+	}
+
+	return message[start:i], i - start, nil
+}
+
+func serializeSimpleError(message string) (string, int, error) {
+	for _, c := range message {
+		if c == '\n' || c == '\r' {
+			return "", 0, fmt.Errorf("CLRF characters are not allowed in simple errors.")
+		}
+	}
+	return fmt.Sprintf("-%s\r\n", message), len(message), nil
+}
+
+func deserializeInteger(message string) (int, int, error) {
+	if message[0] != ':' {
+		return 0, 0, fmt.Errorf("Expected a integer to begin with ':' but got '%c'\n", message[0])
+	}
+
+	start := 1
+	i := start
+	for message[i] != '\r' && message[i+1] != '\n' {
+		i++
+	}
+
+	num, err := strconv.ParseInt(message[start:i], 10, 32)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return int(num), i - start, nil
+}
+
+func serializeInteger(num int) (string, int, error) {
+	message := fmt.Sprintf(":%d\r\n", num)
+	return message, len(message) - 2, nil
+}
+
 func serialize() {
 	return
 }
 
 func main() {
-	return
+	res, l, err := serializeSimpleString("hel\nlo\r")
+	fmt.Println(res)
+	fmt.Println(l)
+	fmt.Println(err)
 }
--- redis_test.go
+++ redis_test.go
@@ -3,9 +3,169 @@ package main
 import "testing"
 
 func TestDeserializeSimpleString(t *testing.T) {
-	input := "+OK\r\n"
-	want := "OK"
-	if result, _, _ := deserializeSimpleString(input); result != want {
-		t.Errorf("Got '%s' but expected '%s'.", result, want)
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		// the table itself
+		{"Should deserialize simple 'OK'", "+OK\r\n", "OK"},
+		{"Should deserialize simple 'Hello World'", "+Hello World\r\n", "Hello World"},
+		{"Should deserialize string with special chars 'Hello\\\tWorld'", "+Hello\\\tWorld\r\n", "Hello\\	World"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ans, _, _ := deserializeSimpleString(test.input)
+			if ans != test.want {
+				t.Errorf("Got '%s' but expected '%s'.", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSerializeSimpleString(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		// the table itself
+		{"Should serialize simple 'OK'", "OK", "+OK\r\n"},
+		{"Should serialize simple 'Hello World'", "Hello World", "+Hello World\r\n"},
+		{"Should serialize string with special chars 'Hello\\\tWorld'", "Hello\\	World", "+Hello\\\tWorld\r\n"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ans, _, _ := serializeSimpleString(test.input)
+			if ans != test.want {
+				t.Errorf("Got '%s' but expected '%s'.", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSerializeSimpleStringInvalid(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		// the table itself
+		{"Should fail to serialize string with CLRF chars 'O\nK\r'", "O\nK\r", ""},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, _, err := serializeSimpleString(test.input)
+			if err == nil {
+				t.Errorf("Got no error but expected failure.")
+			}
+		})
+	}
+}
+
+func TestDeserializeSimpleError(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		// the table itself
+		{"Should deserialize simple error 'ERR'", "-ERR unknown command 'asdf'\r\n", "ERR unknown command 'asdf'"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ans, _, _ := deserializeSimpleError(test.input)
+			if ans != test.want {
+				t.Errorf("Got '%s' but expected '%s'.", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSerializeSimpleError(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		// the table itself
+		{"Should serialize simple error 'ERR'", "ERR unknown command 'asdf'", "-ERR unknown command 'asdf'\r\n"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ans, _, _ := serializeSimpleError(test.input)
+			if ans != test.want {
+				t.Errorf("Got '%s' but expected '%s'.", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSerializeSimpleErrorInvalid(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		// the table itself
+		{"Should fail to serialize error with CLRF chars 'ERR\\n'", "ERR\n", ""},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, _, err := serializeSimpleError(test.input)
+			if err == nil {
+				t.Errorf("Got no error but expected failure.")
+			}
+		})
+	}
+}
+
+func TestDeserializeInteger(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  int
+	}{
+		// the table itself
+		{"Should deserialize unsigned integer", ":1000\r\n", 1000},
+		{"Should deserialize negative integer", ":-1000\r\n", -1000},
+		{"Should deserialize positive integer", ":+1000\r\n", 1000},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ans, _, _ := deserializeInteger(test.input)
+			if ans != test.want {
+				t.Errorf("Got '%d' but expected '%d'.", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSerializeInteger(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input int
+		want  string
+	}{
+		// the table itself
+		{"Should serialize unsigned integer", 1000, ":1000\r\n"},
+		{"Should serialize negative integer", -1000, ":-1000\r\n"},
+		{"Should serialize positive integer", +1000, ":1000\r\n"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ans, _, _ := serializeInteger(test.input)
+			if ans != test.want {
+				t.Errorf("Got '%s' but expected '%s'.", ans, test.want)
+			}
+		})
 	}
 }
